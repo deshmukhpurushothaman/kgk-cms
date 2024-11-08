12a65b95dd0c0883851c7c0a11d5fcae57d66a42
Deshmukh P <deshmukhpurushothaman@gmail.com>
2024-11-08 16:22:13 +0530

    setup api's to handle images in database

---
+++ app/api/images/create/route.ts
@@ -0,0 +1,34 @@
+import { db } from '@/db';
+import { imageSlider, imageSliderLink } from '@/db/schema';
+import { NextResponse } from 'next/server';
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const storedImage = await db
+      .insert(imageSlider)
+      .values({
+        images: body.images,
+      })
+      .returning();
+    const storedImageLink = await db
+      .insert(imageSliderLink)
+      .values({
+        entityType: body.type,
+        imageSliderId: storedImage[0].id,
+        entityId: body.entityId,
+      })
+      .returning();
+    return NextResponse.json({ status: 200, message: 'Successful' });
+  } catch (error: any) {
+    console.log('Error creating/storing images in images->create ', error);
+    return NextResponse.json(
+      {
+        message: error?.message,
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
